f08ed3e4296136e203038f6aebc1a4d2d0c4b6f1
Corbin Cutter <Corbin@Lisas-iMac.lan>
2023-02-02 18:29:51 -0500

    SCHEMA

--- db/schema.sql
+++ db/schema.sql
@@ -4,33 +4,36 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department VARCHAR(30),
-    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL
+    salary DECIMAL,
     department_id INT,
-     FOREIGN KEY(department_id)
+    INDEX dep_ind (department_id),
+    CONSTRAINT fk_department
+    FOREIGN KEY(department_id)
     REFERENCES department(id)
-    ON DELETE SET NULL,
-    PRIMARY KEY (id)
+    ON DELETE CASCADE,
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT  PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT,
+    INDEX role_ind (role_id),
+    CONSTRAINT fk_role
     FOREIGN KEY(role_id) 
     REFERENCES role(id) 
-    ON DELETE SET NULL,
-    CONSTRAINT selfkey 
+    ON DELETE CASCADE,
+    manager_id INT,
+    INDEX man_ind (manager_id),
+    CONSTRAINT fk_manager
     FOREIGN KEY (manager_id) 
-    REFERENCES employee(id),
-    PRIMARY KEY (id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL,
 );
