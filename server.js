102681980f3548116b8b4a1e8a5ad9377c130750
Corbin Cutter <Corbin@Lisas-iMac.lan>
2023-02-03 16:48:22 -0500

    Functions done

--- server.js
+++ server.js
@@ -284,9 +284,87 @@ function employeeQuestions(employeeOptions) {
     });
 };
 
+// Update an employee in employee_db
 
 function updateAnEmployee() {
+    var query =`SELECT 
+        e.id, e.first_name, e.last_name, r.title, d.department AS department, r.salary, 
+        CONCAT(m.first_name, ' ', m.last_name) AS manager
+        FROM employee e
+        JOIN role r
+        ON e.role_id = r.id
+        JOIN department d
+        ON d.id = r.department_id
+        JOIN employee m
+        ON m.id = e.manager_id`
 
+    db.query(query, function(err, res) {
+        if (err) throw err;
+
+        const whichEmployee = res.map(({ id, first_name, last_name }) => ({
+            value: id, name: `${first_name} ${last_name}`
+        })); 
+
+        console.table(res);
+        console.log("employeeArray To Update!\n")
+    
+        whatRole(whichEmployee);
+
+    });
+};
+
+function whatRole(whichEmployee) {
+    var query = `SELECT r.id, r.title, r.salary FROM role r`
+
+    let whichRole;
+
+    db.query(query, function(err, res) {
+        if (err) throw err;
+
+        whichRole = res.map(({ id, title, salary }) => ({
+            value: id, title: `${title}`, salary: `${salary}`
+        }));
+
+        console.table(res);
+
+        finalizeEmployeeUpdate(whichEmployee, whichRole);
+    });
+};
+
+function finalizeEmployeeUpdate(whichEmployee, whichRole) {
+    inquirer.prompt([
+        {
+          type: "list",
+          name: "employeeId",
+          message: "Which employee's role do you want to update?",
+          choices: whichEmployee
+        },
+        {
+          type: "list",
+          name: "roleId",
+          message: "Which role do you want to update that employee to?",
+          choices: whichRole
+        },
+      ])
+      .then(function (answer) {
+  
+        var query = `UPDATE employee SET role_id = ? WHERE id = ?`
+
+        db.query(query,
+          [ 
+            answer.roleId,  
+            answer.employeeId
+          ],
+
+          function (err, res) {
+            if (err) throw err;
+  
+            console.table(res);
+            console.log(res.affectedRows + "Updated successfully!");
+  
+            employerQuestions();
+          });
+      });
 };
 
 function finish() {
