6bfe8ea0934dae21896f50efa5aa2f5e6c9fe9a2
Corbin Cutter <Corbin@Lisas-iMac.lan>
2023-02-04 23:01:12 -0500

    schema help didnt work, went back to old schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,39 +1,68 @@
+-- DROP DATABASE IF EXISTS employee_db;
+-- CREATE DATABASE employee_db;
+
+-- USE employee_db;
+
+-- CREATE TABLE department (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     department VARCHAR(30),
+-- );
+
+-- CREATE TABLE role (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--    title VARCHAR(30),
+--     salary DECIMAL,
+--     department_id INT,
+--     INDEX dep_ind (department_id),
+--     FOREIGN KEY(department_id)
+--     REFERENCES department(id)
+--     ON DELETE CASCADE,
+-- );
+
+-- CREATE TABLE employee (
+--     id INT NOT NULL AUTO_INCREMENT  PRIMARY KEY,
+--     first_name VARCHAR(30),
+--     last_name VARCHAR(30),
+--     role_id INT,
+--     INDEX role_ind (role_id),
+--     FOREIGN KEY(role_id) 
+--     REFERENCES role(id) 
+--     ON DELETE CASCADE,
+--     manager_id INT,
+--     INDEX man_ind (manager_id),
+--     FOREIGN KEY (manager_id) 
+--     REFERENCES employee(id)
+ --    ON DELETE SET NULL,
+-- );
+
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
-
 USE employee_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     department VARCHAR(30),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    INDEX dep_ind (department_id),
-    CONSTRAINT fk_department
     FOREIGN KEY(department_id)
     REFERENCES department(id)
-    ON DELETE CASCADE,
+    ON DELETE SET NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT  PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    INDEX role_ind (role_id),
-    CONSTRAINT fk_role
-    FOREIGN KEY(role_id) 
-    REFERENCES role(id) 
-    ON DELETE CASCADE,
     manager_id INT,
-    INDEX man_ind (manager_id),
-    CONSTRAINT fk_manager
-    FOREIGN KEY (manager_id) 
-    REFERENCES employee(id)
-    ON DELETE SET NULL,
-);
+    FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT selfkey FOREIGN KEY (manager_id) REFERENCES employee(id),
+    PRIMARY KEY (id)
+)
